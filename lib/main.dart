6813f91ee1424e5a17049409d7a0308882dc2b95
ganeshshetty98 <shettyganeshprasad1998@gmail.com>
2020-11-01 20:57:35 +0530

    Integrated Login and Register Page with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
-import 'package:ProfessionConnect/LoginUI.dart';
-import 'package:flutter/material.dart';
+import 'package:ProfessionConnect/models/user.dart';
+import 'package:ProfessionConnect/screens/wrapper.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: LoginPage(),
-  ));
+import 'package:ProfessionConnect/services/auth.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+      ),
+    );
+  }
 }
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,33 @@
+import 'package:ProfessionConnect/models/user.dart';
+import 'package:ProfessionConnect/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    final data = Provider.of<User>(context);
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Profession Connect'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+        body: Text(data.email + data.uid),
+      ),
+    );
+  }
+}
