c4448f668264ca9e2d25d1327419887e5e379e13
ganeshshetty98 <shettyganeshprasad1998@gmail.com>
2020-11-03 12:09:56 +0530

    Changed db to store user data in Firestore Database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,18 +1,41 @@
+import 'package:ProfessionConnect/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 class DatabaseService {
   final String uid;
+
   DatabaseService({this.uid});
+  Map data;
+  addData() {
+    Map<String, dynamic> newData = {
+      "name": "shetty",
+      "email": "gs00619@gmail.com"
+    };
+    CollectionReference collectionReference =
+        Firestore.instance.collection('users');
+    collectionReference.document('gs00619@gmail.com').setData(newData);
+  }
 
   // collection reference
   final CollectionReference userCollection =
       Firestore.instance.collection('userdata');
 
-  Future<void> updateUserData(String sugars, String name, int strength) async {
-    return await userCollection.document(uid).setData({
-      'sugars': sugars,
+  // Future<void> updateUserData(String sugars, String name, int strength) async {
+  //   return await userCollection.document(uid).setData({
+  //     'sugars': sugars,
+  //     'name': name,
+  //     'strength': strength,
+  //   });
+  // }
+
+  Future<void> updateUserData({String email, String name, int age}) async {
+    CollectionReference collectionReference =
+        Firestore.instance.collection('users');
+    collectionReference.document(uid).setData({
+      'email': email,
       'name': name,
-      'strength': strength,
+      'age': age,
     });
   }
 }
